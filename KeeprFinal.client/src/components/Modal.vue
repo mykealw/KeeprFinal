9c6fa27268f43e3d4bee83992d12fd5c042ef6a5
Mykeal Wheeler <mykeal.wheeler@gmail.com>
2022-06-05 16:24:40 -0600

    set up modal templates

---
+++ KeeprFinal.client/src/components/Modal.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="modal fade">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-body p-0">
+          <slot name="modal-body"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
